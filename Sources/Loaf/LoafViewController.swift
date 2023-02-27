009c7e00695560098f78b398eed4da000950d417
schmidyy <https://github.com/schmidyy>
2023-02-27 13:32:16 +0300

    p

--- Sources/Loaf/LoafViewController.swift
+++ Sources/Loaf/LoafViewController.swift
@@ -70,7 +70,7 @@ final class LoafViewController: UIViewController {
         
         if loaf.dismissalReason == .interactive {
             let string = loaf.message
-            let range               = NSMakeRange(string.count - 9, 9)
+            let range               = NSMakeRange(string.count - 10, 8)
             let attributedString    = NSMutableAttributedString(string: string)
             
             attributedString.addAttribute(NSAttributedString.Key.underlineStyle, value: NSNumber(value: 1), range: range)
